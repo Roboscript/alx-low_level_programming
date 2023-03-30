97268afc190f9f27a5ed9342709d2632fc19562c
Roboscript <chikovo@gmail.com>
2023-03-30 04:54:37 -0700

    It is the addition of strangeness to beauty that constitutes the romantic character in art

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,21 +1,15 @@
 #include <stdio.h>
 
-/**
- * main - Entry point.
- * Program prints a[2] = 98 followed by new line
- *
- * Return: Always 0 (Success)
-*/
-
 int main(void)
 {
 	int n;
 	int a[5];
 	int *p;
+
 	a[2] = 1024;
 	p = &n;
 /*
-* Code goes bellow all this.........
+* write your line of code here...
 * Remember:
 * - you are not allowed to use a
 * - you are not allowed to modify p
