aa95aa854f45b9bbea13e26a7ba378b8755791ca
Paul Chikovo <chikovop@gmail.com>
2023-05-11 03:08:59 -0700

    1. Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,21 +10,21 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int txtfile, bnum, length= 0;
+	int txtfile, bnum, length = 0;
 
 	if (filename == NULL)
 		return (-1);
 
 	if (text_content != NULL)
 	{
-		for (length= 0; text_content[length];)
+		for (length = 0; text_content[length];)
 			length++;
 	}
 
-	txtfile= open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	txtfile = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	bnum = write(txtfile, text_content, length);
 
-	if (txtfile== -1 || bnum == -1)
+	if (txtfile == -1 || bnum == -1)
 		return (-1);
 
 	close(txtfile);
